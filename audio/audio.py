971dc4a68cf6466b809aada8a959364d41a1b107
bixind <vbif-98@mail.ru>
2016-01-12 20:38:08 +0400

    added audio

--- audio/audio.py
+++ audio/audio.py
@@ -1,8 +1,17 @@
 import random
+from genres import *
 
 def getAudio(com, vk):
     offset = random.randint(1, 100)
-    resp = vk.method('audio.getPopular', {'offset' : offset, 'count' : 3})
+    values = {'offset' : offset, 'count' : 3}
+    genre = None
+    for val in com.args:
+        ps = val.split('_')
+        if len(ps) > 1 and 'жанр' == ps[0]:
+            genre = genres.get(ps[1], None)
+    if genre is not None:
+        values['genre_id'] = genre
+    resp = vk.method('audio.getPopular', values)
     track = resp[0]
     attachment = 'audio{}_{}'.format(track['owner_id'], track['id'])
     return {'attachment' : attachment}
