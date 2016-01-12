c9b47b244a77b5c77d8f0274f6ee1c3ed7e4136d
bixind <vbif-98@mail.ru>
2016-01-12 19:48:42 +0400

    added audio

--- audio/audio.py
+++ audio/audio.py
@@ -4,6 +4,5 @@ def getAudio(com, vk):
     offset = random.randint(1, 100)
     resp = vk.method('audio.getPopular', {'offset' : offset, 'count' : 3})
     track = resp[0]
-    print(resp[0])
     attachment = 'audio{}_{}'.format(track['owner_id'], track['id'])
     return {'attachment' : attachment}
--- vk_api/vk_api_ext.py
+++ vk_api/vk_api_ext.py
@@ -10,7 +10,7 @@ class VkApiExt(VkApi):
     def __init__(self, login=None, password=None, number=None, sec_number=None,
                  token=None, proxies=None, captcha_handler=None,
                  config_filename='vk_config.json',
-                 api_version=None, app_id=4953880, scope=33554431,
+                 api_version='5.44', app_id=4953880, scope=33554431,
                  client_secret=None):
         super().__init__(login, password, number, sec_number,
                  token, proxies, captcha_handler,
@@ -18,7 +18,7 @@ class VkApiExt(VkApi):
                  api_version, app_id, scope,
                  client_secret)
         self.methodLock = threading.Lock()
-        self.id = self.method('users.get')[0]['uid']
+        self.id = self.method('users.get')[0]['id']
         self.guid = 0
 
     def method(self, method, values=None, captcha_sid=None, captcha_key=None):
@@ -37,7 +37,7 @@ class VkApiExt(VkApi):
         ids = r['items']
         names = self.method('users.get', {'user_ids' : ",".join(map(str, ids))})
         for user in names:
-            self.users[int(user['uid'])] = user
+            self.users[int(user['id'])] = user
         self.logged = self.method('users.get')[0]
 
     def get_user(self, id):
