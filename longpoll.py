890a55fb4d4599c8991024ff34c4ad83c62b2d0e
bixind <vbif-98@mail.ru>
2016-01-13 16:52:43 +0400

    some bugfixes

--- longpoll.py
+++ longpoll.py
@@ -10,33 +10,33 @@ import logging
 class Handler:
     def __init__(self, commands = {}):
         self.commands = commands
+
     def handle(self, upd):
         if upd[0] in self.commands:
             self.commands[upd[0]](upd)
 
+
 def longpoll(vk, handler):
+    while True:
+        l = []
         v = vk.method('messages.getLongPollServer')
         ts = v['ts']
         key = v['key']
         wait = 25
         mode = 2
-        l = []
-        while True:
-            try:
-                data = urlencode({'act':'a_check', 'key' : key, 'ts' : ts, 'wait' : wait, 'mode' : mode})
-                s = urlopen('http://' + v['server'] + '?' + data)
-                l = json.loads(s.read().decode('utf-8'))
-                ts = l['ts']
-                for upd in l['updates']:
-                    handler.handle(upd)
-                vk.method('account.setOnline', {'void' : 0})
-            except HTTPError as e:
-                logging.error(e)
-                v = vk.method('messages.getLongPollServer')
-                ts = v['ts']
-                key = v['key']
-            except Exception as e:
-                logging.exception(l)
-                v = vk.method('messages.getLongPollServer')
-                ts = v['ts']
-                key = v['key']
+        try:
+            while True:
+                    data = urlencode({'act':'a_check', 'key' : key, 'ts' : ts, 'wait' : wait, 'mode' : mode})
+                    s = urlopen('http://' + v['server'] + '?' + data)
+                    l = json.loads(s.read().decode('utf-8'))
+                    if 'failed' in l:
+                        logging.warning(l)
+                        break
+                    ts = l['ts']
+                    for upd in l['updates']:
+                        handler.handle(upd)
+                    vk.method('account.setOnline', {'void' : 0})
+        except HTTPError as e:
+            logging.error(e)
+        except Exception as e:
+            logging.exception(l)
