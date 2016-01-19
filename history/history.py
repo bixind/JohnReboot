242c88e6d243b654cd3ea056267f737930143c8d
bixind <vbif-98@mail.ru>
2016-01-19 20:41:54 +0400

    some arch

--- history/history.py
+++ history/history.py
@@ -6,10 +6,17 @@ import time
 import datetime as date
 import random
 from historycontrol import historyLock
+import os.path
+
+day = 60 * 60 * 24
+
+mpl.rcdefaults()
+font = {'family': 'stixgeneral',
+        'weight': 'normal'}
+mpl.rc('font', **font)
 
 def makeChart(vk):
     d = date.datetime.now(date.timezone(date.timedelta(hours=4)))
-    day = 60 * 60 * 24
     now = int(time.time())
     tp = time.localtime(now)
     dh = day - (tp.tm_min * 60 + tp.tm_sec)
@@ -42,11 +49,6 @@ def makeChart(vk):
             st += now - pos
         tmid.append([st, id])
     tmid.sort(key = lambda x : x[0])
-
-    mpl.rcdefaults()
-    font = {'family': 'stixgeneral',
-            'weight': 'normal'}
-    mpl.rc('font', **font)
 
     r = vk.method('users.get', {'user_ids' : ','.join(str(u) for u in l)})
     usernames = dict()
@@ -85,9 +87,60 @@ def makeChart(vk):
     plt.axis('off')
     plt.savefig('hist.png', bbox_inches = 'tight')
 
+def makePersonalChart(id):
+    with historyLock, open('days/lastupdate.txt') as f:
+        last_update = int(f.readline())
+    dg = list()
+    cnt = 10
+    while True and cnt > 0:
+        cnt-=1
+        curdname = time.strftime('%Y-%m-%d', time.localtime(last_update ))
+        if not os.path.exists('days/' + curdname + '/' + str(id) + '.txt'):
+            break
+        l = []
+        with historyLock, open('days/' + curdname + '/' + str(id) + '.txt') as f:
+            for s in f:
+                s = s.split()
+                l.append([int(s[0]) - 8, int(s[3]) - last_update])
+        dg.append([curdname, l])
+        last_update -= day
+    plt.figure(1, figsize=(20, len(dg) * 3 + 1))
+    plt.subplot(2, 1, 1)
+    dh = day
+    ch = 0
+    while dh > 0:
+        plt.axvline(dh // 60, ls = 'dashed', color = '#B0B0B0')
+        plt.annotate(str(ch) + ':00', xy = (dh // 60 + 5, 0), fontproperties = fonts.FontProperties(size = 10))
+        dh -= 60*60
+        ch -= 1
+        if ch < 0:
+            ch += 24
+    dx = 0
+    offx = 100
+    for dayname, lt in dg:
+        dx += 1
+        pos = -1
+        usrclr = [random.random(), random.random(), random.random()]
+        usrclrfd = list(min(1, clr + 0.5) for clr in usrclr)
+        plt.axhline(dx, lw = 1, color = usrclrfd)
+        for el in lt:
+            if el[0] == 0 and pos == -1:
+                pos = el[1]
+            elif el[0] == 1 and pos != -1:
+                plt.plot([pos // 60, (el[1]) // 60], [dx, dx], color = usrclr, linewidth=2.0)
+                pos = -1
+        if pos != -1:
+            plt.plot([pos // 60, day // 60], [dx, dx], color = usrclr, linewidth=2.0)
+        plt.annotate(dayname, xy = (-offx, dx + 0.2), fontproperties = fonts.FontProperties(size = 10))
+    # plt.plot([0],[0], 'b', linewidth=2.0)
+    # plt.plot([day // 60],[dx + 1], 'b', linewidth=2.0)
+    # plt.annotate('KEK', xy = (2, 2.2))
+    plt.axis([-offx, day // 60, 0, dx + 1])
+    plt.axis('off')
+    plt.savefig('pershist.png', bbox_inches = 'tight')
+
 def getHistory(args, vk):
     makeChart(vk)
     vu = upl.VkUpload(vk)
     r = vu.photo_messages(photos = ['hist.png'])
     return {'attachment' : 'photo' + str(r[0]['owner_id']) + '_' + str(r[0]['id'])}
-
--- historycontrol.py
+++ historycontrol.py
@@ -11,9 +11,13 @@ with historyLock, open('days/lastupdate.txt') as f:
 def statusChange(upd):
     global last_update
     with historyLock:
-        with open('history.txt', 'a') as f:
-            print(*(upd + [round(time.time())]), file = f)
         now = round(time.time())
+        with open('history.txt', 'a') as f:
+            print(*(upd + [now]), file = f)
+        curdname = time.strftime('%Y-%m-%d', time.localtime(now))
+        fp.ensure_dir('days/' + curdname)
+        with open('days/' + curdname + '/' + str(id) + '.txt', 'a') as f:
+            print(*(upd + [now]), file = f)
         newhist = []
         if now > last_update + day:
             l = dict()
@@ -24,19 +28,10 @@ def statusChange(upd):
                     if id not in l:
                         l[id] = []
                     l[id].append(s)
-            while now > last_update + day:
-                curdname = time.strftime('%Y-%m-%d', time.localtime(last_update))
-                fp.ensure_dir('days/' + curdname)
-                for id in l:
-                    with open('days/' + curdname + '/' + str(id) + '.txt', 'w') as f:
-                        for el in l[id]:
-                            if int(el[-1]) < last_update + day:
-                                print(*el, file = f)
-                last_update += day
             with open('history.txt', 'w') as f:
                 for id in l:
                     for el in l[id]:
                         if int(el[-1]) >= now - day:
                             print(*el, file = f)
             with open('days/lastupdate.txt', 'w') as f:
-                print(last_update, file = f)
+                print(now, file = f)
